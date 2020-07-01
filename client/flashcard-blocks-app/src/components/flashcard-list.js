b6a10e3044e0b791b121c70a01b5c9529c75f1d8
Robert Maxwell <robertjamesmaxwell@gmail.com>
2020-07-01 18:07:38 -0500

    Fetching data from backend

--- client/flashcard-blocks-app/src/components/flashcard-list.js
+++ client/flashcard-blocks-app/src/components/flashcard-list.js
@@ -1,8 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Flashcard from "./flashcard";
 import "./flashcard-list.css";
 
-const FlashcardList = ({ flashcards }) => {
+const FlashcardList = () => {
+  const [flashcards, setFlashcards] = useState([]);
+
+  // fetch("http://localhost:5000/flashcard-blocks", {
+  //   mode: "no-cors",
+  //   Accept: "application/json",
+  // }).then((res) => res.json());
+
+  useEffect(() => {
+    fetch("http://localhost:5000/flashcard-blocks")
+      .then((res) => res.json())
+      .then(console.log);
+  }, [flashcards]);
+
   return (
     <div className='flashcard-grid'>
       <Flashcard></Flashcard>
